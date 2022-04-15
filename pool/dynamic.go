7f8e80294c6485391b29d9b019aaddd137e5dbce
Matt Van Gundy <mattvangundy@google.com>
2022-04-15 11:33:11 -0700

    Add license to pool files.
    It was commited without an appropriate license comment.

--- pool/dynamic.go
+++ pool/dynamic.go
@@ -1,3 +1,17 @@
+// Copyright 2021 Google LLC
+//
+// Licensed under the Apache License, Version 2.0 (the "License");
+// you may not use this file except in compliance with the License.
+// You may obtain a copy of the License at
+//
+//     https://www.apache.org/licenses/LICENSE-2.0
+//
+// Unless required by applicable law or agreed to in writing, software
+// distributed under the License is distributed on an "AS IS" BASIS,
+// WITHOUT WARRANTIES OR CONDITIONS OF ANY KIND, either express or implied.
+// See the License for the specific language governing permissions and
+// limitations under the License.
+
 // Package pool provides an object pool that trades off the cost of creation
 // versus retention. It is meant to avoid the pessimal behaviour (see [issue
 // 23199]) seen when using a regular sync.Pool with objects of dynamic sizes;
