3fe86ddc3606757ea516919f454830c8430a6d0e
Стив <kenethra.74@gmail.com>
2022-06-29 20:10:40 -0600

    resolve

--- jar/backup.go
+++ jar/backup.go
@@ -23,7 +23,6 @@ import (
 // backupFile makes copies JARs file in the backup folder
 func backupFile(jarPath, jarSource, jarFile string) error {
 	pathdst := filepath.Join(jarPath, jarFile+".bak")
-
 	_, err := copyJars(jarSource, pathdst)
 	if err != nil {
 		return err
